8abb65a473cb12b0f256ebaa2ff713d047961e2a
Wyatt Paquin <paquinwyatt@gmail.com>
2017-10-15 17:52:10 -0400

    finished lab 2

--- week2/lab/routes/index.js
+++ week2/lab/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var util = require('util');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { pagetitle: 'Wyatts IndexPage' });
 });
+router.get('/index', function (req, res, next) {
+  res.render('index', { pagetitle: 'Wyatts IndexPage' });
+});
+router.get('/form', function (req, res, next) {
+  res.render('form', { pagetitle: 'Wyatts FormPage' });
+});
+router.post('/form', function (req, res, next) {
+  
+  res.render('result', { pagetitle: 'Wyatts ResultPage', email: req.body.email, name: req.body.name, comments: req.body.comments });
+});
+router.get('/about', function (req, res, next) {
+  res.render('about', { pagetitle: 'Wyatts AboutPage' });
+});
+
 
 module.exports = router;

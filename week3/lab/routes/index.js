f7d552c5d4c4e83d3d14e2f990707c1c709bf396
Wyatt Paquin <paquinwyatt@gmail.com>
2017-10-16 19:31:37 -0400

    started work with lab 3

---
+++ week3/lab/routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var debug = require('debug')('lab:index');
+
+var numbers = [1,2,3,4,5,6,7,8];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Lab3', numbers: numbers });
+});
+
+router.post('/', function(req, res, next) {
+  res.render('index', { title: 'Lab3', numbers: numbers });
+
+  debug(req.body.numselect);
+});
+
+module.exports = router;

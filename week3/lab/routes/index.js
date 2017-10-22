6ece43a90259fe833cce147f26bb6203fd4aff7f
Wyatt <Wyatt@DESKTOP-ON0S4R1>
2017-10-22 18:31:01 -0400

    finished part 1

--- week3/lab/routes/index.js
+++ week3/lab/routes/index.js
@@ -2,17 +2,16 @@ var express = require('express');
 var router = express.Router();
 var debug = require('debug')('lab:index');
 
-var numbers = [1,2,3,4,5,6,7,8];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Lab3', numbers: numbers });
 });
 
-router.post('/', function(req, res, next) {
-  res.render('index', { title: 'Lab3', numbers: numbers });
-
+router.post('/', function (req, res, next) {
   debug(req.body.numselect);
+  res.render('colorgrid', { title: 'Lab3', selectednumber: req.body.numselect });
 });
 
 module.exports = router;

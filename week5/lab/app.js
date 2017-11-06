30bdb580a1b0b90d3cad6fdca2152ea71a4f0563
Wyatt Paquin <paquinwyatt@gmail.com>
2017-11-06 00:11:36 -0500

    finished lab 5

--- week5/lab/app.js
+++ week5/lab/app.js
@@ -16,6 +16,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+
+//dont know what this is but for some reason 
+//i was getting an access control error
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
